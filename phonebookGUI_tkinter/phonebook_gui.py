80e1211a18e903e3d177d719f40e4f64cc774472
Mohammad Shafi <41709286+rshafi2@users.noreply.github.com>
2019-12-27 14:31:57 -0500

    Update phonebook_gui.py

--- phonebookGUI_tkinter/phonebook_gui.py
+++ phonebookGUI_tkinter/phonebook_gui.py
@@ -51,18 +51,18 @@ def load_gui(self):
     #Define the listbox with a scrollbar and grid them
     self.scrollbar1 = Scrollbar(self.master,orient=VERTICAL)
     self.lstList1 = Listbox(self.master,exportselection=0,yscrollcommand=self.scrollbar1.set)
-    self.lstList1.bind('<<ListboxSelect>>',lambda event: drill50_phonebook_func.onSelect(self,event))
+    self.lstList1.bind('<<ListboxSelect>>',lambda event: phonebook_func.onSelect(self,event))
     self.scrollbar1.config(command=self.lstList1.yview)
     self.scrollbar1.grid(row=1,column=5,rowspan=7,columnspan=1,padx=(0,0),pady=(0,0),sticky=N+E+S)
     self.lstList1.grid(row=1,column=2,rowspan=7,columnspan=3,padx=(0,0),pady=(0,0),sticky=N+E+S+W)
     
-    self.btn_add = tk.Button(self.master,width=12,height=2,text='Add',command=lambda: drill50_phonebook_func.addToList(self))
+    self.btn_add = tk.Button(self.master,width=12,height=2,text='Add',command=lambda: phonebook_func.addToList(self))
     self.btn_add.grid(row=8,column=0,padx=(25,0),pady=(45,10),sticky=W)
-    self.btn_update = tk.Button(self.master,width=12,height=2,text='Update',command=lambda: drill50_phonebook_func.onUpdate(self))
+    self.btn_update = tk.Button(self.master,width=12,height=2,text='Update',command=lambda: phonebook_func.onUpdate(self))
     self.btn_update.grid(row=8,column=1,padx=(15,0),pady=(45,10),sticky=W)
-    self.btn_delete = tk.Button(self.master,width=12,height=2,text='Delete',command=lambda: drill50_phonebook_func.onDelete(self))
+    self.btn_delete = tk.Button(self.master,width=12,height=2,text='Delete',command=lambda: phonebook_func.onDelete(self))
     self.btn_delete.grid(row=8,column=2,padx=(15,0),pady=(45,10),sticky=W)
-    self.btn_close = tk.Button(self.master,width=12,height=2,text='Close',command=lambda: drill50_phonebook_func.ask_quit(self))
+    self.btn_close = tk.Button(self.master,width=12,height=2,text='Close',command=lambda: phonebook_func.ask_quit(self))
     self.btn_close.grid(row=8,column=4,columnspan=1,padx=(15,0),pady=(45,10),sticky=E)
 
     phonebook_func.create_db(self)
